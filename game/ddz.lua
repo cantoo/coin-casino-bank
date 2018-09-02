3624960d9d8b2942115c30ceb632f1f10cd58ef6
Chen Tao <cantoo523@gmail.com>
2018-09-02 14:35:47 +0800

    Update ddz.lua

--- game/ddz.lua
+++ game/ddz.lua
@@ -1,1 +1,33 @@
 
+local _M = {
+	hands = {}
+}
+
+local players = {}
+
+local function play(hand)
+	for _, player in ipairs(players) do 
+		table.insert(player, hand)
+	end
+end
+
+function _M:join()
+	local player = {}
+	table.insert(players, player)
+	return player
+end
+
+function _M:main()
+	while true
+	do
+		for _, hand in ipairs(hands) do
+			play(hand)
+		end
+
+		_M.hands = {}
+		ngx.sleep(0)
+	end
+end
+
+
+return _M
